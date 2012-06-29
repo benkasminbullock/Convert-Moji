964c2333d61f4894c59b40afd7ee465a4a5131ba
Ben Bullock <benkasminbullock@gmail.com>
2012-06-30 02:05:46 +0900

    Use Perl::Build

--- build.pl
+++ build.pl
@@ -1,26 +1,8 @@
 #!/home/ben/software/install/bin/perl
 use warnings;
 use strict;
-use Getopt::Long;
+use Perl::Build;
 
-GetOptions (
-    "clean" => \my $clean,
+perl_build (
+
 );
-if ($clean) {
-    clean ();
-}
-else {
-    system ("perl Makefile.PL > /dev/null;make > /dev/null;make test");
-}
-
-exit;
-
-sub clean
-{
-    if (-f "Makefile") {
-        system ("make clean > /dev/null");
-    }
-    if (-f "Makefile.old") {
-        unlink "Makefile.old" or die $!;
-    }
-}
